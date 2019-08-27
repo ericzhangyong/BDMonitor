fc634d27ce3c308c3352f39df7aabb08eeafe398
zhangyong <zhangyong2@beadwallet.com>
2019-08-27 16:56:56 +0800

    dddddd

---
+++ Classess/test.h
@@ -0,0 +1,17 @@
+//
+//  test.h
+//  testSdddd
+//
+//  Created by eric on 2019/8/27.
+//  Copyright © 2019 eric. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classess/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  testSdddd
+//
+//  Created by eric on 2019/8/27.
+//  Copyright © 2019 eric. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
